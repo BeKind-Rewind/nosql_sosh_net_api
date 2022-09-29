5e3d61193b3992797f8abecc2a122e6736c2fad4
Amanda Perry <challenge641@gmail.com>
2022-09-28 19:15:58 -0500

    added routes, models, and controllers

--- models/Thought.js
+++ models/Thought.js
@@ -1,24 +1,71 @@
-// Thought
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-// thoughtText
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true
+    },
+    userName: { // not creating new user; the one that  made the post
+      type: String,
+      required: true,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
 
-// String
-// Required
-// Must be between 1 and 280 characters
-// createdAt
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    userName: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
 
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
-// username (The user that created this thought)
+// get total count of thoughts and reactions on retrieval
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reaction.reduce(
+    (total, reaction) => total + reaction.reactions.length + 1, 0);
+});
 
-// String
-// Required
-// reactions (These are like replies)
+const Thought = model('Thought', ThoughtSchema);
 
-// Array of nested documents created with the reactionSchema
-
-
-// ------Schema Settings------
-
-// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,27 +1,59 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-// User
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    createdBy: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    //   get: createdAtVal => dateFormat(createdAtVal)
+    // },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
 
-// username
+// get total count of thoughts and reactions on retrieval
+UserSchema.virtual('friendCount').get(function () {
+  return this.thoughts.reduce(
+    (total, friends) => total + friends.reactions.length + 1, 0);
+});
 
-// String
-// Unique
-// Required
-// Trimmed
-// email
+const User = model('User', UserSchema);
 
-// String
-// Required
-// Unique
-// Must match a valid email address (look into Mongoose's matching validation)
-// thoughts
-
-// Array of _id values referencing the Thought model
-// friends
-
-// Array of _id values referencing the User model (self-reference)
-
-
-
-// ------Schema Settings------
-
-// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+module.exports = User;
